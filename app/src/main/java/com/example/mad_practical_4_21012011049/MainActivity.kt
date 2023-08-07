f7d5bfcd9a77ab9ef5edf1279fd86beda4fe4ef4
student <student@gnu.ac.in>
2023-08-07 08:55:27 +0530

    Practical 4

--- app/src/main/java/com/example/mad_practical_4_21012011049/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012011049/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mad_practical_4_21012011049
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login:Button = findViewById(R.id.login)
+        login.setOnClickListener {
+            Intent(this@MainActivity, LoginActivity::class.java).also { startActivity(it) }
+        }
+
     }
 }
